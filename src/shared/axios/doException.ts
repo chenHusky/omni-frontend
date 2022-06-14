3da6fe9b2e1d433c610bfc4b8922ae128ce6afaa
chenHusky <377075038@qq.com>
2022-06-14 14:53:03 +0800

    错误信息处理

--- src/shared/axios/doException.ts
+++ src/shared/axios/doException.ts
@@ -1,11 +1,18 @@
 import type { AxiosError } from 'axios';
+import { h } from 'vue';
 import { useDialog } from '../utils/useDialog';
 import { RequestConfigMap } from './interface';
 
 export default (err: AxiosError) => {
   const { response, config } = err;
   if ((config as RequestConfigMap).$doException !== 'false') {
-    const errMsg = response?.data.title || err.message;
+    let errContent = '';
+    try {
+      errContent = JSON.parse(response?.data.data)?.error;
+    } catch (error) {
+      errContent = response?.data.data;
+    }
+    const errMsg = h('div', [h('div', response?.data.title || err.message), h('div', errContent)]);
     errMsg;
     const { open } = useDialog();
     open({
